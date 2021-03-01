9fabfda0f4c586c98960d82533de5d0a1f2e170a
Youngsun <youngsunhere@gmail.com>
2021-03-01 18:09:57 +0900

    [MOD] modularized version

---
+++ 02_s2s/data/preprocess.py
@@ -0,0 +1,53 @@
+import spacy
+from torchtext.datasets import Multi30k
+from torchtext.data import Field, BucketIterator
+from hparams import hparams
+
+spacy_fr = spacy.load('fr_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
+
+
+
+def tokenize_fr(text):
+    """
+    Tokenizes German text from a string into a list of strings (tokens) and reverses it
+    """
+    return [tok.text for tok in spacy_fr.tokenizer(text)][::-1]
+
+def tokenize_en(text):
+    """
+    Tokenizes English text from a string into a list of strings (tokens)
+    """
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+def create_dataset(device):
+	SRC = Field(tokenize = tokenize_fr, 
+			init_token = '<sos>', 
+			eos_token = '<eos>', 
+			lower = True)
+
+	TRG = Field(tokenize = tokenize_en, 
+			init_token = '<sos>', 
+			eos_token = '<eos>', 
+			lower = True)
+
+
+	train_data, valid_data, test_data = Multi30k.splits(exts = ('.fr', '.en'), 
+													fields = (SRC, TRG))
+	SRC.build_vocab(train_data, min_freq = 2)
+	TRG.build_vocab(train_data, min_freq = 2)
+
+	#여기에 안 넣는 방법?
+	hparams.TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+
+	hparams.INPUT_DIM = len(SRC.vocab)
+	hparams.OUTPUT_DIM = len(TRG.vocab)
+	
+	train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+	(train_data, valid_data, test_data), batch_size = hparams.BATCH_SIZE, device = device)
+
+	return train_iterator,valid_iterator,test_iterator,hparams
+
+if __name__=='__main__':
+	train_iterator,valid_iterator,test_iterator,hparams,=create_dataset()
---
+++ 02_s2s/trainer.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+import random
+import math
+import time
+
+class Seq2SeqTrain():
+	def __init__(self,model,hparams):
+		self.model=model
+		self.optimizer = optim.Adam(self.model.parameters())
+		self.criterion = nn.CrossEntropyLoss(ignore_index = hparams.TRG_PAD_IDX)
+		self.hparams=hparams
+
+	def init_weights(self,m):
+		for name, param in m.named_parameters():
+			nn.init.uniform_(param.data, -0.08, 0.08)
+
+	def count_parameters(self):
+		return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+
+	def train(self,model, iterator, optimizer, criterion, clip):
+		
+		model.train()
+		
+		epoch_loss = 0
+		
+		for i, batch in enumerate(iterator):
+			
+			src = batch.src
+			trg = batch.trg
+			
+			optimizer.zero_grad()
+			
+			output = model(src, trg)
+			
+			#trg = [trg len, batch size]
+			#output = [trg len, batch size, output dim]
+			
+			output_dim = output.shape[-1]
+			
+			output = output[1:].view(-1, output_dim)
+			trg = trg[1:].view(-1)
+			
+			#trg = [(trg len - 1) * batch size]
+			#output = [(trg len - 1) * batch size, output dim]
+			
+			loss = criterion(output, trg)
+			
+			loss.backward()
+			
+			torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+			
+			optimizer.step()
+			
+			epoch_loss += loss.item()
+			
+		return epoch_loss / len(iterator)
+
+	def evaluate(self,model, iterator, criterion):
+		
+		model.eval()
+		
+		epoch_loss = 0
+		
+		with torch.no_grad():
+		
+			for i, batch in enumerate(iterator):
+
+				src = batch.src
+				trg = batch.trg
+
+				output = model(src, trg, 0) #turn off teacher forcing
+
+				#trg = [trg len, batch size]
+				#output = [trg len, batch size, output dim]
+
+				output_dim = output.shape[-1]
+				
+				output = output[1:].view(-1, output_dim)
+				trg = trg[1:].view(-1)
+
+				#trg = [(trg len - 1) * batch size]
+				#output = [(trg len - 1) * batch size, output dim]
+
+				loss = criterion(output, trg)
+				
+				epoch_loss += loss.item()
+			
+		return epoch_loss / len(iterator)
+
+	def epoch_time(self,start_time, end_time):
+		elapsed_time = end_time - start_time
+		elapsed_mins = int(elapsed_time / 60)
+		elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+		return elapsed_mins, elapsed_secs
+
+	def run(self,train_iterator,valid_iterator):
+		print(f'The model has {self.count_parameters():,} trainable parameters')
+
+		self.model.apply(self.init_weights)
+
+		best_valid_loss = float('inf')
+
+		for epoch in range(self.hparams.N_EPOCHS):
+			
+			start_time = time.time()
+			
+			train_loss = self.train(self.model, train_iterator, self.optimizer, self.criterion, self.hparams.CLIP)
+			valid_loss = self.evaluate(self.model, valid_iterator, self.criterion)
+			
+			end_time = time.time()
+			
+			epoch_mins, epoch_secs = self.epoch_time(start_time, end_time)
+			
+			if valid_loss < best_valid_loss:
+				best_valid_loss = valid_loss
+				torch.save(self.model.state_dict(), f'{self.hparams.save_dir}/fr2en.pt')
+			
+			print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+			print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+			print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
+
